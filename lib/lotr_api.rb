cb78e73ac4bb17f464294f9b3360e771ea851efa
Wurdsmith <wurdsmithing@gmail.com>
2021-02-02 00:57:40 -0600

    Updates API class to create a class variable of unique characters that have quotes.

--- lib/lotr_api.rb
+++ lib/lotr_api.rb
@@ -1,2 +1,25 @@
+require 'httparty'
+require 'pry'
 class Lotr_api
+    
+#@@character_hash = HTTParty.get('https://the-one-api.dev/v2/character', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
+@@query = HTTParty.get('https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfe9e/quote', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
+
+
+
+
+
+
+
+unique_characters_with_quotes = []
+
+@@query.find do |key, value|
+    value.collect do |quote| 
+        binding.pry
+        if quote["character"] == String
+       unique_characters_with_quotes << quote["character"]
+     end
+    end
+end
+
 end
---
+++ lib/tempCodeRunnerFile.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+require 'pry'
+class Lotr_api
+
+#Setting class variables equal to a list of quotes and characters, each of which needs to be cut down so that I can only include characters with quotes in the final cli app.
+@@character_hash = HTTParty.get('https://the-one-api.dev/v2/character', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
+@@quote_hash = HTTParty.get('https://the-one-api.dev/v2/quote', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
+@@unique_characters_with_quotes = []
+@@characters_with_quotes = {}
+
+#Creates a list of unique character IDs from a very large list of movie quotes. Repeated character IDs will not be included because we will be building our character list with these values.
+@@quote_hash.each do |key, value|
+    if value.class != Integer
+    value.collect do |quote| 
+       @@unique_characters_with_quotes << quote["character"] unless @@unique_characters_with_quotes.include? quote["character"]
+     end
+    end
+end
+binding.pry
+end
