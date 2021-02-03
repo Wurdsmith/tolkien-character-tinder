dafa1d978c4a58e3d90b5e2f73853dd760f0fe40
Wurdsmith <wurdsmithing@gmail.com>
2021-02-03 05:03:20 -0600

    Filters API into a useable character list with unique stats. Each character has at least one movie quote that can be returned when the used begins a conversation with matched character.

--- lib/lotr_api.rb
+++ lib/lotr_api.rb
@@ -6,15 +6,23 @@ class Lotr_api
 @@character_hash = HTTParty.get('https://the-one-api.dev/v2/character', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
 @@quote_hash = HTTParty.get('https://the-one-api.dev/v2/quote', headers: {"Authorization" => "Bearer Bw68B-g1vre-EFiDydiG"})
 @@unique_characters_with_quotes = []
-@@characters_with_quotes = {}
+@@character_list_with_stats = []
 
 #Creates a list of unique character IDs from a very large list of movie quotes. Repeated character IDs will not be included because we will be building our character list with these values.
 @@quote_hash.each do |key, value|
     if value.class != Integer
     value.collect do |quote| 
        @@unique_characters_with_quotes << quote["character"] unless @@unique_characters_with_quotes.include? quote["character"]
-     end
+      end
     end
+  end
+
+@@character_hash.each do |key, value|
+  value.each do |character_stats|
+    if @@unique_characters_with_quotes.include? character_stats["_id"]
+      @@character_list_with_stats << character_stats
+    end
+  end
+  binding.pry
 end
-binding.pry
 end
--- lib/tinder_cli.rb
+++ lib/tinder_cli.rb
@@ -1,7 +1,17 @@
 class Tinder_cli
 
     def welcome
-        puts "Thank you for signing up for Tolkien Tinder!"       
+        puts "Thank you for signing up for Tolkien Tinder!" 
+        puts "There are many fine matches to be had in Middle Earth."
+        self.race_preference_prompt
+    end
+
+    def race_preference_promt
+        puts "Please select input the three fictional races that most interest you."
+        puts "Your choices: Human, Elf, Hobbit, Dwarf, Orc, Wizard"
+        puts "Be sure to separate your choices with a comma! *Example: Human, Hobbit, Elf."
+        puts "Happy hunting!"
+        input = gets.strip
     end
 end
 
