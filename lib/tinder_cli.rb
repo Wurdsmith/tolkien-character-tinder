7e469bd2f9b87b1481308dba5832c2fb2233ab81
Wurdsmith <wurdsmithing@gmail.com>
2021-02-04 02:46:56 -0600

    adds the foundation for the 'yes/no' 'swipe right/swipe left functionality in the CLI.

--- lib/tinder_cli.rb
+++ lib/tinder_cli.rb
@@ -15,15 +15,29 @@ class Tinder_cli
         input_3 = gets.strip
         character_preferences = Lotr_api.get_characters_by_race(input_1, input_2, input_3)
         self.character_viewer(character_preferences)
-        binding.pry
     end
 
     def character_viewer(character_preferences)
         # bring selected characters array by invoking get_characters_by_race method(array of characters hashes)
-        arr = [{}, {}, {}]
-        puts "Your first match is..."
-        # show info of the first el: arr[0] = {1}
-        arr[1]
+        count = 0
+        index = 0
+        while count < character_preferences.length
+            puts "Your next potential match is: #{character_preferences [index].name}. Would you like to know more?"
+            puts "Type 'yes' to learn more, or type 'no' see your next potential match!"
+            input = gets.strip
+                if input == "yes"
+                    puts "#{character_preferences [index].name}"
+                    puts "Race = #{character_preferences [index].race}"
+                    puts "Gender = #{character_preferences [index].gender}"
+                    puts "Realm = #{character_preferences [index].realm}"
+                    puts "Height = #{character_preferences [index].height}"
+                    puts "Hair color = #{character_preferences [index].hair}"
+
+                elsif input == "no"
+                    count += 1
+                    index += 1
+                end
+        end
     end
 
 end
